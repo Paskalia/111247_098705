1e8497eaa19dc4ff2b7fb8cd1a102632141a92ab
Grace-Joy <33892212+Grace-Joy@users.noreply.github.com>
2024-01-12 01:09:51 +0300

    Update RecipeRepository.kt

--- Paskalia/RecipeAppDB/RecipeAppDB/app/src/main/java/com/example/recipeappdb/RecipeRepository.kt
+++ Paskalia/RecipeAppDB/RecipeAppDB/app/src/main/java/com/example/recipeappdb/RecipeRepository.kt
@@ -4,4 +4,15 @@ import androidx.lifecycle.LiveData
 
 // RecipeRepository.kt
 
+class RecipeRepository(private val recipeDao: RecipeDao) {
+
+    val allRecipes: LiveData<List<Recipe>> = recipeDao.getAllRecipes()
+
+    suspend fun insert(recipe: Recipe) {
+        recipeDao.insert(recipe)
+    }
+    suspend fun delete(recipe: Recipe) {
+        recipeDao.delete(recipe)
+    }
+}
 
