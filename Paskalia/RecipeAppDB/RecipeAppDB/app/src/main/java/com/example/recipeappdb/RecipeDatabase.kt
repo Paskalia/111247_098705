470f6c6fb880721d6c71865b3ecd84ba66929b62
Grace-Joy <33892212+Grace-Joy@users.noreply.github.com>
2024-01-12 01:08:18 +0300

    Update RecipeDatabase.kt

--- Paskalia/RecipeAppDB/RecipeAppDB/app/src/main/java/com/example/recipeappdb/RecipeDatabase.kt
+++ Paskalia/RecipeAppDB/RecipeAppDB/app/src/main/java/com/example/recipeappdb/RecipeDatabase.kt
@@ -7,3 +7,26 @@ import androidx.room.RoomDatabase
 
 
 
+/ RecipeDatabase.kt
+
+@Database(entities = [Recipe::class], version = 1, exportSchema = false)
+abstract class RecipeDatabase : RoomDatabase() {
+    abstract fun recipeDao(): RecipeDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: RecipeDatabase? = null
+
+        fun getDatabase(context: Context): RecipeDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    RecipeDatabase::class.java,
+                    "recipe_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
